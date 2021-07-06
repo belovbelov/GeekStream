2fdcc9d666ff779f25d54ebffad90edf4f45fed1
Anton Belov <itoha32@gmail.com>
2021-07-06 19:26:48 +0300

    Add User Context

---
+++ src/GeekStream.Infrastructure/Data/UserDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GeekStream.Core.Entities;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekStream.Infrastructure.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
